702d25c276c628e5b014c83c7a400f2c911f2eb4
Sagiri Izumi <58117773+Pluykan@users.noreply.github.com>
2021-02-25 20:17:38 +0500

    Update script.js

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -69,17 +69,7 @@ $(document).ready(function() {
 		}
       });
       
-      $(window).on('load', function () {
-		$(".items").isotope({
-			filter: '*',
-			animationOptions: {
-				duration: 1500,
-				easing: 'linear',
-				queue: false
-			}
-		});
-	});
-
+     
 
 
 
